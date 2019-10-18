3209edcbf785b736cd7ad5fbf815dec3d8d8ff6f
Herless Alvarado <herlessalvarado@Herlesss-MacBook-Pro.local>
2019-10-18 12:38:08 -0500

    first two

--- src/seedu/addressbook/data/person/Address.java
+++ src/seedu/addressbook/data/person/Address.java
@@ -6,8 +6,12 @@ import seedu.addressbook.data.exception.IllegalValueException;
  * Represents a Person's address in the address book.
  * Guarantees: immutable; is valid as declared in {@link #isValidAddress(String)}
  */
-public class Address {
+public class Address implements ReadAdress{
 
+    private Block block;
+    private Street street;
+    private Unit unit;
+    private PostalCode postalCode;
     public static final String EXAMPLE = "123, some street";
     public static final String MESSAGE_ADDRESS_CONSTRAINTS = "Person addresses can be in any format";
     public static final String ADDRESS_VALIDATION_REGEX = ".+";
@@ -15,11 +19,34 @@ public class Address {
     public final String value;
     private boolean isPrivate;
 
-    /**
-     * Validates given address.
-     *
-     * @throws IllegalValueException if given address string is invalid.
-     */
+    public Address(Block block, Street street, Unit unit, PostalCode postalCode){
+        this.block = block;
+        this.street = street;
+        this.unit = unit;
+        this.postalCode = postalCode;
+        this.value = block.toString() + street.toString() + unit.toString() + postalCode.toString();
+    }
+
+    @Override
+    public Block getBlock(){
+        return block;
+    }
+
+    @Override
+    public Street getStreet(){
+        return street;
+    }
+
+    @Override
+    public Unit getUnit(){
+        return unit;
+    }
+
+    @Override
+    public PostalCode getPostalCode(){
+        return postalCode;
+    }
+
     public Address(String address, boolean isPrivate) throws IllegalValueException {
         String trimmedAddress = address.trim();
         this.isPrivate = isPrivate;
